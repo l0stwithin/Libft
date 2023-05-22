1d8578aaefe56d9ffa3573149abe835aabb5eade
sdutta <sdutta@student.wolfsburg.de>
2023-05-22 23:44:03 +0200

    updated header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sdutta <sdutta@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 12:20:38 by sdutta            #+#    #+#             */
-/*   Updated: 2023/05/22 20:51:48 by sdutta           ###   ########.fr       */
+/*   Updated: 2023/05/22 23:41:12 by sdutta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-void 	*ft_memset(void *s, int c, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
